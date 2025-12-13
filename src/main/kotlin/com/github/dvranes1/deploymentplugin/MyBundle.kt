6b7c279546b5a39b1d0be8c133212974a56ede64
GitHub Action <action@github.com>
2025-12-13 12:00:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/dvranes1/deploymentplugin/MyBundle.kt
+++ src/main/kotlin/com/github/dvranes1/deploymentplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dvranes1.deploymentplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dvranes1/deploymentplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/dvranes1/deploymentplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.dvranes1.deploymentplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
